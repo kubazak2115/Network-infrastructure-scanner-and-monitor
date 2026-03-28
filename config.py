d8c1d8fa50b0240ed68522149887a6d09137c735
kubazak <kubazak@student.agh.edu.pl>
2026-03-28 21:48:24 +0100

    added Docker

--- config.py
+++ config.py
@@ -1,8 +1,10 @@
+import os
+
 DEVICE = {
     'device_type': 'linux',
-    'host': '10.0.1.99',
-    'username': 'user',
-    'password': 'user'
+    'host':        os.getenv('TARGET_HOST', '10.0.1.99'),
+    'username':    os.getenv('TARGET_USER', 'user'),
+    'password':    os.getenv('TARGET_PASS', 'user'),
 }
 
 API_HOST = '0.0.0.0'
