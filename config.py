864829f807067de976b0d2eb4556ab12f7091b89
kubazak <kubazak@student.agh.edu.pl>
2026-03-29 11:40:46 +0200

    added multi-host support and cyclic metrics collection

--- config.py
+++ config.py
@@ -1,11 +1,19 @@
 import os
 
-DEVICE = {
+DEVICES = [
+    {
     'device_type': 'linux',
     'host':        os.getenv('TARGET_HOST', '10.0.1.99'),
     'username':    os.getenv('TARGET_USER', 'user'),
     'password':    os.getenv('TARGET_PASS', 'user'),
+},
+{
+    'device_type': 'linux',
+    'host':        os.getenv('TARGET_HOST', '10.0.1.100'),
+    'username':    os.getenv('TARGET_USER', 'debianuser'),
+    'password':    os.getenv('TARGET_PASS', '1319'),
 }
-
+]
 API_HOST = '0.0.0.0'
 API_PORT = 5000
+INTERVAL_SECONDS = 20
